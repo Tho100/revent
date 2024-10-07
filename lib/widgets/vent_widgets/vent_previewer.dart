8c7fdd1768c76c7e1ee6f473612d749b0a63fcb1
Tho100 <nfrealyt@gmail.com>
2024-10-07 15:08:21 +0800

    Created separated widgets function for vent previewer codebase

--- lib/widgets/vent_widgets/vent_previewer.dart
+++ lib/widgets/vent_widgets/vent_previewer.dart
@@ -13,8 +13,83 @@ class VentPreviewer extends StatelessWidget {
     super.key
   });
 
+  Widget _buildCommunityAndCreatorHeader() {
+    return Row(
+      children: [
+
+        Container(
+          width: 30,
+          height: 30,
+          decoration: const BoxDecoration(
+            color: ThemeColor.white,
+            shape: BoxShape.circle
+          )
+        ),
+
+        const SizedBox(width: 10),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Text(
+              'motivation',
+              style: GoogleFonts.inter(
+                color: ThemeColor.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 15,
+              ),
+            ),
+
+            const SizedBox(height: 2.5),
+
+            Text(
+              'By dan_isgay',
+              style: GoogleFonts.inter(
+                color: ThemeColor.thirdWhite,
+                fontWeight: FontWeight.w800,
+                fontSize: 12
+              ),
+            ),
+
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width-90,
+      child: Text(
+        title,
+        style: GoogleFonts.inter(
+          color: ThemeColor.white,
+          fontWeight: FontWeight.w800,
+          fontSize: 16
+        ),
+        overflow: TextOverflow.fade,
+        softWrap: false,
+      ),
+    );
+  }
+
+  Widget _buildBodyText() {
+    return Text(
+      bodyText,
+      style: GoogleFonts.inter(
+        color: ThemeColor.secondaryWhite,
+        fontWeight: FontWeight.w800,
+        fontSize: 12.5
+      ),
+      overflow: TextOverflow.ellipsis,
+      maxLines: 2
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO: Add likes/comments buttons
     return Container(
       height: 175,
       decoration: BoxDecoration(
@@ -31,77 +106,15 @@ class VentPreviewer extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
 
-            Row(
-              children: [
-
-                Container(
-                  width: 30,
-                  height: 30,
-                  decoration: const BoxDecoration(
-                    color: ThemeColor.white,
-                    shape: BoxShape.circle
-                  )
-                ),
-
-                const SizedBox(width: 10),
-
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-
-                    Text(
-                      'motivation',
-                      style: GoogleFonts.inter(
-                        color: ThemeColor.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 15,
-                      ),
-                    ),
-
-                    const SizedBox(height: 2.5),
-
-                    Text(
-                      'By dan_isgay',
-                      style: GoogleFonts.inter(
-                        color: ThemeColor.thirdWhite,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 12
-                      ),
-                    ),
-
-                  ],
-                ),
-              ],
-            ),
+            _buildCommunityAndCreatorHeader(),
 
             const SizedBox(height: 14),
 
-            SizedBox(
-              width: MediaQuery.of(context).size.width-90,
-              child: Text(
-                title,
-                style: GoogleFonts.inter(
-                  color: ThemeColor.white,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 16
-                ),
-                overflow: TextOverflow.fade,
-                softWrap: false,
-              ),
-            ),
+            _buildTitle(context),
 
             const SizedBox(height: 12),
-      
-            Text(
-              bodyText,
-              style: GoogleFonts.inter(
-                color: ThemeColor.secondaryWhite,
-                fontWeight: FontWeight.w800,
-                fontSize: 12.5
-              ),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2
-            ),
+
+            _buildBodyText(),
       
           ],
         ),
