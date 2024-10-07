7109bd1ae0cc9b9ff4a9ff079f601a964302e4e6
Tho100 <nfrealyt@gmail.com>
2024-10-07 21:36:04 +0800

    Added like/comment button on vent-previewer widget

--- lib/widgets/vent_widgets/vent_previewer.dart
+++ lib/widgets/vent_widgets/vent_previewer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:revent/themes/theme_color.dart';
@@ -6,12 +7,26 @@ class VentPreviewer extends StatelessWidget {
 
   final String title;
   final String bodyText;
+  final int totalLikes;
+  final int totalComments;
 
-  const VentPreviewer({
+  VentPreviewer({
     required this.title,
     required this.bodyText,
+    required this.totalLikes,
+    required this.totalComments,
     super.key
   });
+
+  final defaultActionButtonsStyle = ElevatedButton.styleFrom(
+    foregroundColor: ThemeColor.thirdWhite,
+    backgroundColor: ThemeColor.black,
+    shape: const StadiumBorder(
+      side: BorderSide(
+        color: ThemeColor.thirdWhite
+      )
+    ),
+  );
 
   Widget _buildCommunityAndCreatorHeader() {
     return Row(
@@ -87,11 +102,82 @@ class VentPreviewer extends StatelessWidget {
     );
   }
 
+  Widget _buildLikeButton() {
+    return SizedBox(
+      height: 34,
+      child: ElevatedButton(
+        style: defaultActionButtonsStyle,
+        onPressed: () => () {},
+          child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+  
+            Transform.translate(
+              offset: const Offset(0, -1),
+              child: const Icon(
+                CupertinoIcons.heart_fill, 
+                color: Color.fromARGB(255, 127, 15, 50),
+                size: 18.5, 
+              ),
+            ),
+  
+            const SizedBox(width: 6), 
+  
+            Text(
+              totalLikes.toString(),
+              style: GoogleFonts.inter(
+                color: ThemeColor.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 13.5,
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCommentsButton() {
+    return SizedBox(
+      height: 34,
+      child: ElevatedButton(
+        style: defaultActionButtonsStyle,
+        onPressed: () => () {},
+          child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+    
+            Transform.translate(
+              offset: const Offset(0, -1),
+              child: const Icon(
+                CupertinoIcons.chat_bubble, 
+                color: ThemeColor.white,
+                size: 18, 
+              ),
+            ),
+    
+            const SizedBox(width: 6), 
+    
+            Text(
+              totalComments.toString(),
+              style: GoogleFonts.inter(
+                color: ThemeColor.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 13.5,
+              ),
+            ),
+    
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Add likes/comments buttons
     return Container(
-      height: 175,
+      height: 200,
       decoration: BoxDecoration(
         color: ThemeColor.black,
         border: Border.all(
@@ -116,6 +202,23 @@ class VentPreviewer extends StatelessWidget {
 
             _buildBodyText(),
       
+            const SizedBox(height: 22),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Row(
+                children: [
+                      
+                  _buildLikeButton(),
+                      
+                  const SizedBox(width: 8),
+                      
+                  _buildCommentsButton(),
+                      
+                ],
+              ),
+            ),
+
           ],
         ),
       ),
