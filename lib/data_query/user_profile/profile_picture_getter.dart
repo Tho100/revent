efc1efbfe1457266a9fb8e7b6fe091d5930cad9d
Tho100 <nfrealyt@gmail.com>
2024-10-13 20:51:06 +0800

    Moved profilepicturegetter to separated class

---
+++ lib/data_query/user_profile/profile_picture_getter.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:get_it/get_it.dart';
+import 'package:revent/connection/revent_connect.dart';
+import 'package:revent/model/extract_data.dart';
+import 'package:revent/provider/user_data_provider.dart';
+
+class ProfilePictureGetter {
+
+  Future<Uint8List> getProfilePicture({String? username}) async {
+    
+    final userData = GetIt.instance<UserDataProvider>();
+
+    final conn = await ReventConnect.initializeConnection();
+
+    const query = "SELECT profile_picture FROM user_profile_info WHERE username = :username";
+    final params = {
+      'username': username!.isNotEmpty ? username : userData.username
+    };
+
+    final result = await conn.execute(query, params);
+
+    final pfpData = ExtractData(rowsData: result).extractStringColumn('profile_picture');
+    return base64Decode(pfpData[0]);
+    
+  }
+
+}
