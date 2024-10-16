24105a387dbba6ccb6a23d4a3ad394bfc1be13ad
Tho100 <nfrealyt@gmail.com>
2024-10-16 20:23:36 +0800

    Renamed profile_page -> my_profile_page

---
+++ lib/pages/user_profile_page.dart
@@ -0,0 +1,196 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:revent/data_query/user_profile/profile_data_setup.dart';
+import 'package:revent/helper/navigate_page.dart';
+import 'package:revent/pages/edit_profile_page.dart';
+import 'package:revent/provider/navigation_provider.dart';
+import 'package:revent/model/update_navigation.dart';
+import 'package:revent/provider/profile_data_provider.dart';
+import 'package:revent/provider/user_data_provider.dart';
+import 'package:revent/themes/theme_color.dart';
+import 'package:revent/widgets/buttons/custom_outlined_button.dart';
+import 'package:revent/widgets/profile_picture.dart';
+
+class UserProfilePage extends StatefulWidget {
+
+  final String username;
+  final Uint8List pfpData;
+
+  const UserProfilePage({
+    required this.username,
+    required this.pfpData,
+    super.key
+  });
+
+  @override
+  State<UserProfilePage> createState() => UserProfilePageState();
+
+}
+
+class UserProfilePageState extends State<UserProfilePage> {
+
+  final navigationIndex = GetIt.instance<NavigationProvider>();
+  final userData = GetIt.instance<UserDataProvider>();
+  final profileData = GetIt.instance<ProfileDataProvider>();
+
+  Future<void> _pageOnRefresh() async {
+
+    profileData.clearProfileData();
+
+    await ProfileDataSetup().setup(username: userData.username);
+
+  }
+
+  Widget _buildUsername() {
+    return Text(
+      userData.username,
+      style: GoogleFonts.inter(
+        color: ThemeColor.white,
+        fontWeight: FontWeight.w800,
+        fontSize: 20.5
+      ),
+    );
+  }
+
+  Widget _buildBio(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.65,
+      child: Text(
+        profileData.bio,
+        style: GoogleFonts.inter(
+          color: ThemeColor.secondaryWhite,
+          fontWeight: FontWeight.w700,
+          fontSize: 14
+        ),
+        textAlign: TextAlign.center,
+        maxLines: 3,
+      ),
+    );
+  }
+
+  Widget _buildEditProfileButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: CustomOutlinedButton(
+        customWidth: MediaQuery.of(context).size.width * 0.45,
+        customHeight: MediaQuery.of(context).size.height * 0.050,
+        customFontSize: 15.5,
+        text: 'Edit profile',
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const EditProfilePage())
+        )
+      ),
+    );
+  }
+
+  Widget _buildPopularityHeader(String header, int value) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        Text(
+          value.toString(),
+          style: GoogleFonts.inter(
+            color: ThemeColor.white,
+            fontWeight: FontWeight.w800,
+            fontSize: 20
+          ),
+        ),
+
+        const SizedBox(height: 4),
+
+        Text(
+          header,
+          style: GoogleFonts.inter(
+            color: ThemeColor.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 14.5
+          ),
+        ),
+
+      ],
+    );
+  }
+
+  Widget _popularityWidgets(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+    
+          _buildPopularityHeader('followers', 10),
+          _buildPopularityHeader('posts', 5),
+          _buildPopularityHeader('following', 12),
+    
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 35.0),
+      child: Column(
+        children: [
+
+          const SizedBox(height: 27),
+    
+          ProfilePictureWidget(
+            pfpData: profileData.profilePicture,
+          ),
+          
+          const SizedBox(height: 12),
+    
+          _buildUsername(),
+    
+          const SizedBox(height: 8),
+    
+          _buildBio(context),
+    
+          const SizedBox(height: 25),
+    
+          _buildEditProfileButton(context),
+
+          const SizedBox(height: 40),
+
+          _popularityWidgets(context),
+
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState(); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    navigationIndex.setPageIndex(4);
+    return WillPopScope(
+      onWillPop: () async {
+        NavigatePage.homePage();
+        return true;
+      },
+      child: Scaffold(
+        body: RefreshIndicator(
+          onRefresh: () async => await _pageOnRefresh(),
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: _buildBody(context)
+          ),
+        ),
+        bottomNavigationBar: UpdateNavigation(
+          context: context,
+        ).showNavigationBar(),
+      ),
+    );
+  }
+  
+}
