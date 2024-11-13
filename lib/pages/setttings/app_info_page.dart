b1bafc03d69f9f444d02ed6d54da2b61c757b600
Tho100 <nfrealyt@gmail.com>
2024-11-13 20:51:28 +0800

    Allow user to clear app-cache

--- lib/pages/setttings/app_info_page.dart
+++ lib/pages/setttings/app_info_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:revent/model/app_cache.dart';
 import 'package:revent/pages/setttings/app_info/pp_page.dart';
 import 'package:revent/pages/setttings/app_info/tp_page.dart';
 import 'package:revent/themes/theme_color.dart';
+import 'package:revent/ui_dialog/snack_bar.dart';
 import 'package:revent/widgets/app_bar.dart';
 import 'package:revent/widgets/buttons/main_button.dart';
 import 'package:revent/widgets/buttons/settings_button.dart';
@@ -20,6 +23,22 @@ class AppInfoPage extends StatefulWidget {
 class AppInfoPageState extends State<AppInfoPage> {
   
   final cacheNotifier = ValueNotifier<String>('');
+
+  void _clearAppCache() async {
+    
+    final cacheSizeInMb = await AppCache().cacheSizeInMb();
+    
+    await DefaultCacheManager().emptyCache();
+
+    final tempDir = await getTemporaryDirectory();
+
+    if(tempDir.existsSync()) {
+      await tempDir.delete(recursive: true);
+    }
+
+    SnackBarDialog.temporarySnack(message: "Cleared ${cacheSizeInMb.toDouble().toStringAsFixed(2)}Mb");
+
+  }
 
   void _initializeCacheSize() async {
 
@@ -68,7 +87,7 @@ class AppInfoPageState extends State<AppInfoPage> {
       customWidth: MediaQuery.of(context).size.width * 0.26,
       customHeight: 46,
       text: 'Clear cache', 
-      onPressed: () => print('Cleared')
+      onPressed: () => _clearAppCache()
     );
   }
 
