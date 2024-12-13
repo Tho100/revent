0f53ab6447da8542e1d8c404470b10265b14fa02
Tho100 <nfrealyt@gmail.com>
2024-12-13 14:52:50 +0800

    Improved vent-data getter codebase

--- lib/vent_query/vent_data_getter.dart
+++ lib/vent_query/vent_data_getter.dart
@@ -13,124 +13,62 @@ class VentDataGetter {
 
   Future<Map<String, dynamic>> getVentsData() async {
 
-    final conn = await ReventConnect.initializeConnection();
+    const query = '''
+      SELECT title, body_text, creator, created_at, total_likes, total_comments 
+        FROM vent_info
+    ''';
 
-    const query = 'SELECT title, body_text, creator, created_at, total_likes, total_comments FROM vent_info';
-    
-    final retrievedVents = await conn.execute(query);
-
-    final extractedData = ExtractData(rowsData: retrievedVents);
-
-    final title = extractedData.extractStringColumn('title');
-    final bodyText =  extractedData.extractStringColumn('body_text');
-
-    final creator = extractedData.extractStringColumn('creator');
-
-    final totalLikes = extractedData.extractIntColumn('total_likes');
-    final totalComments = extractedData.extractIntColumn('total_comments');
-
-    final postTimestamp = extractedData
-      .extractStringColumn('created_at')
-      .map((timestamp) => formatPostTimestamp.formatPostTimestamp(DateTime.parse(timestamp)))
-      .toList();
-
-    final isLikedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'liked'
-    );
-
-    final isSavedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'saved'
-    );
-
-    return {
-      'title': title,
-      'body_text': bodyText,
-      'creator': creator,
-      'post_timestamp': postTimestamp,
-      'total_likes': totalLikes,
-      'total_comments': totalComments,
-      'is_liked': isLikedState,
-      'is_saved': isSavedState
-    };
+    return _fetchVentsData(query);
 
   }
 
   Future<Map<String, dynamic>> getFollowingVentsData() async {
 
-    final conn = await ReventConnect.initializeConnection();
-
-    const getFollowingQuery = '''
+    const query = '''
       SELECT v.title, v.body_text, v.creator, v.created_at, v.total_likes, v.total_comments
         FROM vent_info v
           INNER JOIN user_follows_info u ON u.following = v.creator
           WHERE u.follower = :follower
     ''';
 
-    final followingParam = {'follower': userData.user.username};
-    
-    final followingResults = await conn.execute(getFollowingQuery, followingParam);
+    final params = {'follower': userData.user.username};
 
-    final extractedData = ExtractData(rowsData: followingResults);
-
-    final title = extractedData.extractStringColumn('title');
-    final bodyText = extractedData.extractStringColumn('body_text');
-    final creator = extractedData.extractStringColumn('creator');
-    final totalLikes = extractedData.extractIntColumn('total_likes');
-    final totalComments = extractedData.extractIntColumn('total_comments');
-    
-    final postTimestamp = extractedData
-      .extractStringColumn('created_at')
-      .map((timestamp) => formatPostTimestamp.formatPostTimestamp(DateTime.parse(timestamp)))
-      .toList();
-
-    final isLikedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'liked'
-    );
-
-    final isSavedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'saved'
-    );
-
-    return {
-      'title': title,
-      'body_text': bodyText,
-      'creator': creator,
-      'post_timestamp': postTimestamp,
-      'total_likes': totalLikes,
-      'total_comments': totalComments,
-      'is_liked': isLikedState,
-      'is_saved': isSavedState
-    };
+    return _fetchVentsData(query, params: params);
 
   }
 
   Future<Map<String, dynamic>> getSearchVentsData({required String? searchTitleText}) async {
-
-    final conn = await ReventConnect.initializeConnection();
 
     const query = '''
       SELECT title, creator, created_at, total_likes, total_comments 
         FROM vent_info 
         WHERE LOWER(title) LIKE LOWER(:search_text) OR LOWER(body_text) LIKE LOWER(:search_text)
     ''';
-    
-    final searchParam = {'search_text': '%$searchTitleText%'};
 
-    final retrievedVents = await conn.execute(query, searchParam);
-   
-    final extractedData = ExtractData(rowsData: retrievedVents);
+    final params = {'search_text': '%$searchTitleText%'};
+
+    return _fetchVentsData(query, params: params, excludeBodyText: true);
+
+  }
+
+  Future<Map<String, dynamic>> _fetchVentsData(
+    String query, 
+    {Map<String, dynamic>? params, bool excludeBodyText = false}
+  ) async {
+
+    final conn = await ReventConnect.initializeConnection();
+
+    final results = params != null 
+      ? await conn.execute(query, params)
+      : await conn.execute(query);
+
+    final extractedData = ExtractData(rowsData: results);
 
     final title = extractedData.extractStringColumn('title');
 
-    final bodyText = List<String>.generate(title.length, (_) => '');
+    final bodyText = excludeBodyText
+      ? List<String>.generate(title.length, (_) => '')
+      : extractedData.extractStringColumn('body_text');
 
     final creator = extractedData.extractStringColumn('creator');
 
@@ -143,15 +81,11 @@ class VentDataGetter {
       .toList();
 
     final isLikedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'liked'
+      conn: conn, title: title, stateType: 'liked'
     );
 
     final isSavedState = await _ventPostState(
-      conn: conn,
-      title: title,
-      stateType: 'saved'
+      conn: conn, title: title, stateType: 'saved'
     );
 
     return {
@@ -162,7 +96,7 @@ class VentDataGetter {
       'total_likes': totalLikes,
       'total_comments': totalComments,
       'is_liked': isLikedState,
-      'is_saved': isSavedState
+      'is_saved': isSavedState,
     };
 
   }
