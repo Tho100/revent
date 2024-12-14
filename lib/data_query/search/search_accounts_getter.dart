ef1b3bcc229028aea020249b75ab99ce04bfff3b
Tho100 <nfrealyt@gmail.com>
2024-12-14 21:28:49 +0800

    Allows user to search for profile accounts

---
+++ lib/data_query/search/search_accounts_getter.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:revent/connection/revent_connect.dart';
+import 'package:revent/model/extract_data.dart';
+
+class SearchAccountsGetter {
+
+  Future<Map<String, List<dynamic>>> getAccounts({required String searchText}) async {
+
+    final conn = await ReventConnect.initializeConnection();
+
+    const query = 'SELECT username, profile_picture FROM user_profile_info WHERE username LIKE :search_text';
+
+    final param = {'search_text': '%$searchText%'};
+
+    final results = await conn.execute(query, param);
+
+    final extractedData = ExtractData(rowsData: results);
+
+    final usernames = extractedData.extractStringColumn('username');
+
+    final profilePictures = extractedData
+      .extractStringColumn('profile_picture')
+      .map((pfp) => base64Decode(pfp))
+      .toList();
+
+    return {
+      'username': usernames,
+      'profile_pic': profilePictures,
+    };
+
+  }
+
+
+}
