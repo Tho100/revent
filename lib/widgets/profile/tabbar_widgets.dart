aee3ca63cb295770b833fd619ee5450626dfedc6
Tho100 <nfrealyt@gmail.com>
2024-12-16 14:07:41 +0800

    Minor UI changes

--- lib/widgets/profile/tabbar_widgets.dart
+++ lib/widgets/profile/tabbar_widgets.dart
@@ -23,7 +23,7 @@ class ProfileTabBarWidgets {
 
   Widget buildTabBarTabs() {
     return Padding(
-      padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 12.0),
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 12.0),
       child: TabBarView(
         controller: controller,
         children: [
