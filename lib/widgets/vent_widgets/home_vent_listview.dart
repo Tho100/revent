077465da5ce09b15f2a4c77bba19079e3b76b86c
Tho100 <nfrealyt@gmail.com>
2024-12-18 12:49:46 +0800

    Makes home-listview keep-alive

--- lib/widgets/vent_widgets/home_vent_listview.dart
+++ lib/widgets/vent_widgets/home_vent_listview.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:revent/pages/empty_page.dart';
 import 'package:revent/widgets/vent_widgets/default_vent_previewer.dart';
 
-class HomeVentListView extends StatelessWidget {
+class HomeVentListView extends StatefulWidget {
 
   final dynamic provider;
 
@@ -11,6 +11,16 @@ class HomeVentListView extends StatelessWidget {
     required this.provider,
     super.key
   });
+
+  @override
+  State<HomeVentListView> createState() => _HomeVentListViewState();
+
+}
+
+class _HomeVentListViewState extends State<HomeVentListView> with AutomaticKeepAliveClientMixin {
+
+  @override
+  bool get wantKeepAlive => true;
 
   Widget _buildVentPreview(dynamic ventData) {
     return Padding(
@@ -29,7 +39,7 @@ class HomeVentListView extends StatelessWidget {
 
   Widget _buildVentList() {
     
-    final ventDataList = provider.vents;
+    final ventDataList = widget.provider.vents;
 
     return DynamicHeightGridView(
       physics: const AlwaysScrollableScrollPhysics(
@@ -47,12 +57,15 @@ class HomeVentListView extends StatelessWidget {
   }
 
   Widget _buildOnEmpty() {
-    return EmptyPage().customMessage(message: 'Nothing to see here.');
+    return EmptyPage().customMessage(
+      message: 'Nothing to see here.'
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return provider.vents.isEmpty 
+    super.build(context);
+    return widget.provider.vents.isEmpty 
       ? _buildOnEmpty()
       : _buildVentList();
   }
