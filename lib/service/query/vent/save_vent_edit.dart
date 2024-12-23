421434107020430d03d50e88ccb5fe8115951bc1
Tho100 <nfrealyt@gmail.com>
2024-12-23 14:06:09 +0800

    Shows user when a post was last edited

--- lib/service/query/vent/save_vent_edit.dart
+++ lib/service/query/vent/save_vent_edit.dart
@@ -1,3 +1,4 @@
+import 'package:revent/helper/format_date.dart';
 import 'package:revent/helper/get_it_extensions.dart';
 import 'package:revent/main.dart';
 import 'package:revent/service/query/general/base_query_service.dart';
@@ -18,14 +19,16 @@ class SaveVentEdit extends BaseQueryService {
 
     const query = 
       'UPDATE vent_info SET body_text = :new_body WHERE title = :title AND creator = :creator';
-
+    // TODO: Update to params
     final param = {
       'title': title,
       'creator': userData.user.username,
       'new_body': newBody
     };
 
-    await executeQuery(query, param);
+    await executeQuery(query, param).then(
+      (_) async => await _updateLastEdit()
+    );
 
     getIt.activeVentProvider.setBody(newBody);
 
@@ -43,6 +46,27 @@ class SaveVentEdit extends BaseQueryService {
     };
 
     await executeQuery(query, param);
+
+  }
+
+  Future<void> _updateLastEdit() async {
+
+    final dateTimeNow = DateTime.now();
+
+    const query = 
+      'UPDATE vent_info SET last_edit = :last_edit WHERE title = :title AND creator = :creator';
+
+    final params = {
+      'title': title,
+      'creator': userData.user.username,
+      'last_edit': dateTimeNow
+    };
+
+    await executeQuery(query, params);
+
+    final formatTimeStamp = FormatDate().formatPostTimestamp(dateTimeNow);
+
+    getIt.activeVentProvider.setLastEdit(formatTimeStamp);
 
   }
 
