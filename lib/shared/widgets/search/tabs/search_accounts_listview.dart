c4dd352b4a3b037f8f5509febc554c03cc4bbca2
Tho100 <nfrealyt@gmail.com>
2025-01-07 13:57:47 +0800

    Allow user to follow/unfollow profile on search page

--- lib/shared/widgets/search/tabs/search_accounts_listview.dart
+++ lib/shared/widgets/search/tabs/search_accounts_listview.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:revent/model/user/user_follow_actions.dart';
 import 'package:revent/pages/empty_page.dart';
 import 'package:revent/shared/provider/search/search_accounts_provider.dart';
 import 'package:revent/shared/widgets/account_profile.dart';
 
-class SearchAccountsListView extends StatelessWidget {
+class SearchAccountsListView extends StatefulWidget {
 
   const SearchAccountsListView({super.key});
+
+  @override
+  State<SearchAccountsListView> createState() => _SearchAccountsListViewState();
+
+}
+
+class _SearchAccountsListViewState extends State<SearchAccountsListView> {
+
+  final profileActionTextNotifier = ValueNotifier<List<String>>([]);
+
+  Future<void> _followOnPressed(int index, String username, bool follow) async {
+
+    await UserFollowActions(username: username).followUser(follow: follow).then((_) {
+
+      final updatedList = List<String>.from(profileActionTextNotifier.value);
+
+      updatedList[index] = follow ? 'Unfollow' : 'Follow';
+      profileActionTextNotifier.value = updatedList;
+
+    });
+
+  }
 
   Widget _buildListView(SearchAccountsData accountsData) {
     return Padding(
@@ -17,10 +40,27 @@ class SearchAccountsListView extends StatelessWidget {
         ),
         itemCount: accountsData.usernames.length,
         itemBuilder: (_, index) {
-          return AccountProfileWidget(
-            username: accountsData.usernames[index], 
-            pfpData: accountsData.profilePictures[index]
+
+          final username = accountsData.usernames[index];
+          final pfpData = accountsData.profilePictures[index];
+
+          return ValueListenableBuilder(
+            valueListenable: profileActionTextNotifier,
+            builder: (_, text, __) {
+              final currentText = text[index];
+              return AccountProfileWidget(
+                customText: currentText,
+                username: username,
+                pfpData: pfpData,
+                onPressed: () async {
+                  currentText == 'Follow' 
+                    ? await _followOnPressed(index, username, true)
+                    : await _followOnPressed(index, username, false);
+                },
+              );
+            },
           );
+
         },
       ),
     );
@@ -33,11 +73,23 @@ class SearchAccountsListView extends StatelessWidget {
   }
 
   @override
+  void dispose() {
+    profileActionTextNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Consumer<SearchAccountsProvider>(
       builder: (_, accountsData, __) {
 
         final accountsDataList = accountsData.accounts;
+
+        if(accountsDataList.usernames.isNotEmpty) {
+          profileActionTextNotifier.value = List.generate(
+            accountsDataList.usernames.length, (index) => 'Follow'
+          ); 
+        }
 
         return accountsDataList.usernames.isEmpty 
           ? _buildOnEmpty()
