22d4de7f5f6d173280970ae1c002a044ea75e92c
Tho100 <nfrealyt@gmail.com>
2025-02-11 13:35:39 +0800

    Added comments/replies providers to providers-service

--- lib/helper/providers_service.dart
+++ lib/helper/providers_service.dart
@@ -7,6 +7,8 @@ import 'package:revent/shared/provider/search/search_accounts_provider.dart';
 import 'package:revent/shared/provider/search/search_posts_provider.dart';
 import 'package:revent/shared/provider/user_provider.dart';
 import 'package:revent/shared/provider/vent/active_vent_provider.dart';
+import 'package:revent/shared/provider/vent/comments_provider.dart';
+import 'package:revent/shared/provider/vent/replies_provider.dart';
 import 'package:revent/shared/provider/vent/vent_following_provider.dart';
 import 'package:revent/shared/provider/vent/vent_for_you_provider.dart';
 import 'package:revent/shared/provider/vent/vent_trending_provider.dart';
@@ -44,5 +46,17 @@ mixin ProfilePostsProviderService {
 mixin NavigationProviderService {
   
   NavigationProvider get navigationProvider => getIt<NavigationProvider>();
+
+}
+
+mixin CommentsProviderService {
+
+  CommentsProvider get commentsProvider => getIt<CommentsProvider>();
+
+}
+
+mixin RepliesProviderService {
+
+  RepliesProvider get repliesProvider => getIt<RepliesProvider>();
 
 }
