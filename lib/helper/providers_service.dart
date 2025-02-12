2773b6bee319d1cdbab9eb8648205a32b8631254
Tho100 <nfrealyt@gmail.com>
2025-02-12 13:01:51 +0800

    Added LikedSavedProviderService to provider-service

--- lib/helper/providers_service.dart
+++ lib/helper/providers_service.dart
@@ -8,7 +8,9 @@ import 'package:revent/shared/provider/search/search_posts_provider.dart';
 import 'package:revent/shared/provider/user_provider.dart';
 import 'package:revent/shared/provider/vent/active_vent_provider.dart';
 import 'package:revent/shared/provider/vent/comments_provider.dart';
+import 'package:revent/shared/provider/vent/liked_vent_provider.dart';
 import 'package:revent/shared/provider/vent/replies_provider.dart';
+import 'package:revent/shared/provider/vent/saved_vent_provider.dart';
 import 'package:revent/shared/provider/vent/vent_following_provider.dart';
 import 'package:revent/shared/provider/vent/vent_for_you_provider.dart';
 import 'package:revent/shared/provider/vent/vent_trending_provider.dart';
@@ -40,6 +42,13 @@ mixin ProfilePostsProviderService {
 
   ProfilePostsProvider get profilePostsProvider => getIt<ProfilePostsProvider>();
   ProfileSavedProvider get profileSavedProvider => getIt<ProfileSavedProvider>();
+
+}
+
+mixin LikedSavedProviderService {
+
+  LikedVentProvider get likedVentProvider => getIt<LikedVentProvider>();
+  SavedVentProvider get savedVentProvider => getIt<SavedVentProvider>();
 
 }
 
