fea399de18de3d0973d38b807cf538553afb9fca
Tho100 <nfrealyt@gmail.com>
2025-02-12 20:51:48 +0800

    Use providers-service on UserAccountManager

--- lib/model/user/user_account_manager.dart
+++ lib/model/user/user_account_manager.dart
@@ -1,17 +1,16 @@
-import 'package:revent/helper/get_it_extensions.dart';
 import 'package:revent/helper/navigate_page.dart';
-import 'package:revent/main.dart';
+import 'package:revent/helper/providers_service.dart';
 import 'package:revent/model/local_storage_model.dart';
 import 'package:revent/service/query/general/delete_account_data.dart';
 
-class UserAccountManager {
+class UserAccountManager with UserProfileProviderService, ProfilePostsProviderService {
 
   void signOutUser() async {
 
-    getIt.userProvider.clearUserData();
-    getIt.profileProvider.clearProfileData();
-    getIt.profilePostsProvider.clearPostsData();
-    getIt.profileSavedProvider.clearPostsData();
+    userProvider.clearUserData();
+    profileProvider.clearProfileData();
+    profilePostsProvider.clearPostsData();
+    profileSavedProvider.clearPostsData();
 
     await LocalStorageModel().deleteLocalData();
 
