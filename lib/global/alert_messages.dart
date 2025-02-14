2262ad40f9c6f23cf02b193f49c40eae78f760e1
Tho100 <nfrealyt@gmail.com>
2025-02-14 20:20:12 +0800

    Created alert-messages class

---
+++ lib/global/alert_messages.dart
@@ -0,0 +1,9 @@
+class AlertMessages {
+
+  static const deletePost = 'Delete this post?';
+  static const discardPost = 'Discard post?';
+  static const discardComment = 'Discard comment?';
+  static const discardReply = 'Discard reply?';
+  static const discardEdit = 'Discard edit?';
+
+}
