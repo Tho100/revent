8a45da8171d5fa5ce230531718fdde636b254544
Tho100 <nfrealyt@gmail.com>
2025-02-14 20:22:34 +0800

    Added more messages to AlertMessages

--- lib/global/alert_messages.dart
+++ lib/global/alert_messages.dart
@@ -1,6 +1,13 @@
 class AlertMessages {
 
+  static const savedChanges = 'Saved changes.';
+
+  static const wentWrong = 'Something went wrong.';
+
   static const deletePost = 'Delete this post?';
+  static const deleteArchive = 'Delete this archive post?';
+  static const deleteAccount = 'Delete your account?';
+
   static const discardPost = 'Discard post?';
   static const discardComment = 'Discard comment?';
   static const discardReply = 'Discard reply?';
