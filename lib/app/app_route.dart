33972020bc6a3565c45dd92b1c4653eb1e37ae50
Tho100 <nfrealyt@gmail.com>
2025-02-20 20:52:45 +0800

    Created separated enum for AppRoute

--- lib/app/app_route.dart
+++ lib/app/app_route.dart
@@ -1,19 +1,27 @@
 import 'package:revent/helper/get_it_extensions.dart';
 import 'package:revent/main.dart';
 
-class AppRoute {
+enum AppRoute {
+
+  home('/home/'),
+  myProfile('/profile/my_profile/'),
+  userProfile('/profile/user_profile/'),
+  searchResults('/search/search_results/'),
+  likedPosts('/settings/liked_posts/'),
+  savedPosts('/settings/saved_posts/');
+
+  final String path;
+
+  const AppRoute(this.path);
+
+}
+
+class RouteHelper {
 
   static final _navigation = getIt.navigationProvider;
 
-  static const home = '/home/';
-  static const myProfile = '/profile/my_profile/';
-  static const userProfile = '/profile/user_profile/';
-  static const searchResults = '/search/search_results/';
-  static const likedPosts = '/settings/liked_posts/';
-  static const savedPosts = '/settings/saved_posts/';
-
   static bool get isOnProfile =>
-    _navigation.currentRoute == myProfile || 
-    _navigation.currentRoute == userProfile;
-  
+    _navigation.currentRoute == AppRoute.myProfile.path ||
+    _navigation.currentRoute == AppRoute.userProfile.path;
+    
 }
