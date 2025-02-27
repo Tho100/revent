e1542f13e57469ed9f62adef0973dd0c050573db
Tho100 <nfrealyt@gmail.com>
2025-02-27 21:19:46 +0800

    Remove user-social-links data on account delete

--- lib/service/query/general/delete_account_data.dart
+++ lib/service/query/general/delete_account_data.dart
@@ -5,11 +5,12 @@ class DeleteAccountData extends BaseQueryService {
   Future<void> delete({required String username}) async {
 
     const query = '''
-      DELETE ui, upi, upvi, ufi, vi, svi, avi, lvi, ci, cli, cri, rli
+      DELETE ui, upi, upvi, ufi, usl, vi, svi, avi, lvi, ci, cli, cri, rli
       FROM user_information ui
         LEFT JOIN user_profile_info upi ON upi.username = ui.username
         LEFT JOIN user_privacy_info pvi ON upvi.username = ui.username
-        LEFT JOIN user_follows_info ufi ON ufi.follower = ui.username
+        LEFT JOIN user_follows_info ufi ON ufi.follower = ui.username,
+        LEFT JOIN user_social_links usl ON ufi.username = ui.username,
         LEFT JOIN vent_info vi ON vi.creator = ui.username
         LEFT JOIN saved_vent_info svi ON svi.creator = ui.username
         LEFT JOIN archive_vent_info avi ON avi.creator = ui.username
