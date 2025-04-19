e67370042be37e0157b3ea7a4c32bfc5b576b1e2
Tho100 <nfrealyt@gmail.com>
2025-04-19 13:40:16 +0800

    Makes device status-bar and bottom-navigation transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:revent/app/app_widget_restart.dart';
@@ -55,6 +56,12 @@ void initializeLocators() {
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+  ));
 
   await dotenv.load(fileName: '.env');
 
