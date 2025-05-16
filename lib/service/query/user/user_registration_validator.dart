6e5242c4895ae4fda3762483bf5137ab610994c0
Tho100 <nfrealyt@gmail.com>
2025-05-16 13:09:41 +0800

    Created separated class for user-reigstration-validator

---
+++ lib/service/query/user/user_registration_validator.dart
@@ -0,0 +1,42 @@
+import 'package:revent/helper/extract_data.dart';
+import 'package:revent/service/query/general/base_query_service.dart';
+
+class UserRegistrationValidator extends BaseQueryService {
+
+  final String username;
+  final String email;
+
+  UserRegistrationValidator({
+    required this.username, 
+    required this.email
+  });
+
+  Future<Map<String, bool>> verifyUsernameAndEmail() async {
+
+    const query = 
+    '''
+    SELECT
+	    EXISTS (SELECT 1 FROM user_information WHERE username = :username) AS username_exists,
+      EXISTS (SELECT 1 FROM user_information WHERE email = :email) AS email_exists;
+    ''';
+
+    final params = {
+      'username': username,
+      'email': email
+    };
+
+    final results = await executeQuery(query, params);
+
+    final extractedData = ExtractData(rowsData: results);
+
+    final usernameExists = extractedData.extractIntColumn('username_exists')[0] == 1;
+    final emailExists = extractedData.extractIntColumn('email_exists')[0] == 1;
+
+    return {
+      'username_exists': usernameExists,
+      'email_exists': emailExists
+    };
+
+  }
+
+}
