2b41b256b9087fa51f76b278846610f611be750e
Tho100 <nfrealyt@gmail.com>
2025-05-27 13:43:46 +0800

    Makes system navigation bar transparent

--- lib/pages/splash_screen_page.dart
+++ lib/pages/splash_screen_page.dart
@@ -85,7 +85,7 @@ class _SplashScreenState extends State<SplashScreen> with UserProfileProviderSer
       );
 
     } else {
-      splashScreenTimer = Timer(const Duration(milliseconds: 2000), 
+      splashScreenTimer = Timer(const Duration(milliseconds: 2000), // TODO: Update to 1.7s
         () => _navigateToNextScreen()
       );
       
