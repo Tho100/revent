40fc171f145b0c4a80ec092de1279b8a08bd7f90
Tho100 <nfrealyt@gmail.com>
2025-06-10 13:37:55 +0800

    Loads new follower on notification-page

--- lib/pages/navigation/notifications_page.dart
+++ lib/pages/navigation/notifications_page.dart
@@ -41,11 +41,26 @@ class _NotificationsPageState extends State<NotificationsPage> with
     final prefs = await SharedPreferences.getInstance();
 
     final storedLikesJson = prefs.getString('post_like_cache') ?? '{}';
+    final storedFollowersJson = prefs.getString('followers_cache') ?? '{}';
+
     final storedLikes = jsonDecode(storedLikesJson);
+    final storedFollowers = jsonDecode(storedFollowersJson);
+
+    final Map<String, List<dynamic>> combined = {};
 
     if (storedLikes is Map) {
-      notificationNotifier.value = Map<String, List<dynamic>>.from(storedLikes);
+      storedLikes.forEach((title, data) {
+        combined[title] = [data[0], data[1], 'like'];
+      });
     }
+
+    if (storedFollowers is Map) {
+      storedFollowers.forEach((username, data) {
+        combined[username] = [0, data[0], 'follow'];
+      });
+    }
+
+    notificationNotifier.value = combined;
 
   }
 
@@ -96,7 +111,7 @@ class _NotificationsPageState extends State<NotificationsPage> with
 
   }
 
-  Widget _buildHeart() {
+  Widget _buildPostLiked() {
     return Container(
       width: 45,
       height: 45,
@@ -108,18 +123,32 @@ class _NotificationsPageState extends State<NotificationsPage> with
     );
   }
 
-  Widget _buildMainInfo(String title, int likes, String likedAt) {
+  Widget _buildNewFollower() {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.blueAccent
+      ),
+      child: const Icon(CupertinoIcons.person_add, color: Colors.white),
+    );
+  }
+
+  Widget _buildMainInfo(String title, int likes, String likedAt, {String type = 'like'}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
 
         Text(
-          likes == 1 ? 'Someone liked your post' : 'Your post received $likes likes',
+          type == 'follow'
+            ? '$title followed you'
+            : (likes == 1 ? 'Someone liked your post' : 'Your post received $likes likes'),
           style: GoogleFonts.inter(
             color: ThemeColor.contentPrimary,
             fontWeight: FontWeight.w800,
-            fontSize: 16
+            fontSize: 16,
           ),
         ),
 
@@ -129,23 +158,23 @@ class _NotificationsPageState extends State<NotificationsPage> with
           children: [
 
             Text(
-              title,
+              type == 'follow' ? '' : title,
               style: GoogleFonts.inter(
                 color: ThemeColor.contentSecondary,
                 fontWeight: FontWeight.w700,
-                fontSize: 14
+                fontSize: 14,
               ),
               maxLines: 1,
             ),
 
-            const SizedBox(width: 6),
+            if (type != 'follow') const SizedBox(width: 6),
 
             Text(
               likedAt,
               style: GoogleFonts.inter(
                 color: ThemeColor.contentThird,
                 fontWeight: FontWeight.w700,
-                fontSize: 13
+                fontSize: 13,
               ),
             ),
 
@@ -155,44 +184,59 @@ class _NotificationsPageState extends State<NotificationsPage> with
       ],
     );
   }
-
-  Widget _buildNotificationListView(List<String> titlesData, List<int> likesData, List<String> timestamp) {
+// TODO: Refactor this codebase
+  Widget _buildNotificationListView(
+    List<String> titlesData,
+    List<int> likesData,
+    List<String> timestamp,
+    List<String> types
+  ) {
     return RefreshIndicator(
       backgroundColor: ThemeColor.contentPrimary,
       onRefresh: () async => await _refreshNotifications(),
       child: Padding(
         padding: const EdgeInsets.only(top: 12.0),
         child: ListView.builder(
-          physics: const AlwaysScrollableScrollPhysics(
-            parent: BouncingScrollPhysics(),
-          ),
+          physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
           itemCount: titlesData.length,
           itemBuilder: (_, index) {
+
+            final type = types[index];
+
             return InkWellEffect(
-              onPressed: () async => await _navigateToPost(title: titlesData[index]),
+              onPressed: () async { // TODO: bring user to profile when [type] is follower
+                if (type == 'like') {
+                  await _navigateToPost(title: titlesData[index]);
+                }
+              },
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Container(
                   height: 50,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25)
+                    borderRadius: BorderRadius.circular(25),
                   ),
                   child: Row(
                     children: [
-      
-                      _buildHeart(),
-              
+
+                      type == 'like' ? _buildPostLiked() : _buildNewFollower(),
+
                       const SizedBox(width: 12),
-                      
-                      _buildMainInfo(titlesData[index], likesData[index], timestamp[index]),
-              
+
+                      _buildMainInfo(
+                        titlesData[index],
+                        likesData[index],
+                        timestamp[index],
+                        type: type,
+                      ),
+
                       const Spacer(),
-              
+
                       Padding(
                         padding: const EdgeInsets.only(right: 4.0),
                         child: Icon(Icons.arrow_forward_ios, color: ThemeColor.contentThird, size: 18),
-                      )
-                      
+                      ),
+
                     ],
                   ),
                 ),
@@ -210,17 +254,18 @@ class _NotificationsPageState extends State<NotificationsPage> with
       builder: (_, data, __) {
 
         final sortedEntries = data.entries.toList()
-        ..sort((a, b) {
-          final aTime = formatTimestamp.convertRelativeTimestampToDateTime(b.value[1]);
-          final bTime = formatTimestamp.convertRelativeTimestampToDateTime(a.value[1]);
-          return aTime.compareTo(bTime);
-        });
+          ..sort((a, b) {
+            final aTime = formatTimestamp.convertRelativeTimestampToDateTime(b.value[1]);
+            final bTime = formatTimestamp.convertRelativeTimestampToDateTime(a.value[1]);
+            return aTime.compareTo(bTime);
+          });
 
         final titles = sortedEntries.map((e) => e.key).toList();
         final likes = sortedEntries.map((e) => e.value[0] as int).toList();
         final likedAt = sortedEntries.map((e) => e.value[1].toString()).toList();
+        final types = sortedEntries.map((e) => e.value.length > 2 ? e.value[2].toString() : 'like').toList();
 
-        return _buildNotificationListView(titles, likes, likedAt);
+        return _buildNotificationListView(titles, likes, likedAt, types);
 
       },
     );
