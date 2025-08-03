977cb4c63f7976cf9232b7ffd67643040b4af689
Tho100 <nfrealyt@gmail.com>
2025-08-03 12:59:11 +0800

    Renamed data-converter convertToBools function parameters

--- lib/helper/data_converter.dart
+++ lib/helper/data_converter.dart
@@ -11,14 +11,8 @@ class DataConverter {
     return profilePictures.map((pfp) => base64Decode(pfp)).toList();
   }
 
-  /// Converts a list of integer NSFW flags into a list of boolean values.
-  ///
-  /// Each non-zero integer is interpreted as [True], zero as [False].
-  /// 
-  /// [nsfwMarks] A list of integers representing NSFW flags.
- // TODO: Make it more general e.g name rename the parameter to values
-  static List<bool> convertToBools(List<int> nsfwMarks) {    
-    return nsfwMarks.map((isNsfw) => isNsfw != 0).toList();
+  static List<bool> convertToBools(List<int> values) {    
+    return values.map((value) => value != 0).toList();
   }
 
   static int convertBoolToInt(bool value) => value ? 1 : 0;
