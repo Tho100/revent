3a1e0fb07336d3431a890070483a13bb603edc3d
Tho100 <nfrealyt@gmail.com>
2025-08-10 13:51:09 +0800

    Created password-validation unit test

--- lib/security/hash_model.dart
+++ lib/security/hash_model.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
 class HashingModel {
-
+  // TODO: make this static
   String computeHash(String strInput) {
 
     final authByteCase = utf8.encode(strInput);
