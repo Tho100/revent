10703b478db2ba80db87276869c727d44dc9380d
Tho100 <nfrealyt@gmail.com>
2025-08-27 16:31:34 +0800

    Redesigned navigation bar dock

--- lib/shared/widgets/navigation/navigation_bar_dock.dart
+++ lib/shared/widgets/navigation/navigation_bar_dock.dart
@@ -11,11 +11,19 @@ class NavigationBarDock extends StatelessWidget {
   NavigationBarDock({super.key});
   
   final _navigationIcons = {
-    NavigationTabs.home: CupertinoIcons.home,
+    NavigationTabs.home: CupertinoIcons.house,
     NavigationTabs.search: CupertinoIcons.search,
-    NavigationTabs.create: CupertinoIcons.add,
+    NavigationTabs.create: CupertinoIcons.plus,
     NavigationTabs.activity: CupertinoIcons.heart,
     NavigationTabs.profile: CupertinoIcons.person,
+  };
+
+  final _navigationSelectedIcons = {
+    NavigationTabs.home: CupertinoIcons.house_fill,
+    NavigationTabs.search: CupertinoIcons.search,
+    NavigationTabs.create: CupertinoIcons.plus,
+    NavigationTabs.activity: CupertinoIcons.heart_fill,
+    NavigationTabs.profile: CupertinoIcons.person_fill,
   };
 
   final _navigationActions = {
@@ -38,59 +46,69 @@ class NavigationBarDock extends StatelessWidget {
           final page = entry.key;
           final icon = entry.value;
 
-          return Material(
-            color: Colors.transparent,
-            child: Consumer<NavigationProvider>(
-              builder: (_, navigation, __) {
-
-                final isSelected = navigation.currentNavigation == page;
-
-                final iconColor = page == NavigationTabs.create
-                  ? ThemeColor.contentThird
-                  : (isSelected ? ThemeColor.contentPrimary : ThemeColor.contentThird);
-
-                return Stack(
-                  children: [
-
-                    IconButton(
-                      icon: Icon(icon),
-                      color: iconColor,
-                      iconSize: 28,
-                      onPressed: () {
-                        if (page != NavigationTabs.create) {
-                          navigation.setPage(page);
-                        }
-                        _onNavigationButtonPressed(page);
-                      },
-                    ),
-
-                    if (page == NavigationTabs.activity && navigation.showActivityBadge)
-                    Positioned(
-                      top: 9,
-                      right: 8,
-                      child: Container(
-                        width: 14,
-                        height: 14,
-                        decoration: BoxDecoration(
-                          color: ThemeColor.backgroundPrimary,
-                          shape: BoxShape.circle,
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Material(
+              color: Colors.transparent,
+              child: Consumer<NavigationProvider>(
+                builder: (_, navigation, __) {
+          
+                  final isSelected = navigation.currentNavigation == page;
+          
+                  final iconColor = page == NavigationTabs.create
+                    ? ThemeColor.contentThird
+                    : (isSelected ? ThemeColor.contentPrimary : ThemeColor.contentThird);
+          
+                  final selectedIcon = isSelected 
+                    ? _navigationSelectedIcons[page]
+                    : icon;
+          
+                  return Stack(
+                    children: [
+          
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: IconButton(
+                          icon: Icon(selectedIcon),
+                          color: iconColor,
+                          iconSize: 28,
+                          onPressed: () {
+                            if (page != NavigationTabs.create) {
+                              navigation.setPage(page);
+                            }
+                            _onNavigationButtonPressed(page);
+                          },
                         ),
-                        child: Center(
-                          child: Container(
-                            width: 10,
-                            height: 10,
-                            decoration: BoxDecoration(
-                              color: ThemeColor.alert,
-                              shape: BoxShape.circle,
+                      ),
+          
+                      if (page == NavigationTabs.activity && navigation.showActivityBadge)
+                      Positioned(
+                        top: 9,
+                        right: 8,
+                        child: Container(
+                          width: 14,
+                          height: 14,
+                          decoration: BoxDecoration(
+                            color: ThemeColor.backgroundPrimary,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 10,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                color: ThemeColor.alert,
+                                shape: BoxShape.circle,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-
-                  ],
-                );
-              },
+          
+                    ],
+                  );
+                },
+              ),
             ),
           );
         }).toList(),
