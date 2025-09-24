35ec27d8727d1d28ab9c9f338a41c4db07acfa54
Tho100 <nfrealyt@gmail.com>
2025-09-24 13:16:07 +0800

    Minor codebase improvement on pin-vent

--- lib/service/query/vent/pin_vent.dart
+++ lib/service/query/vent/pin_vent.dart
@@ -1,4 +1,3 @@
-import 'package:revent/global/table_names.dart';
 import 'package:revent/shared/api/api_client.dart';
 import 'package:revent/shared/api/api_path.dart';
 import 'package:revent/shared/provider_mixins.dart';
@@ -12,33 +11,38 @@ class PinVent extends BaseQueryService with UserProfileProviderService, ProfileP
 
   Future<Map<String, dynamic>> pin() async {
 
+    final response = await _callPinRequest();
+
+    if (response['status_code'] == 200) {
+      _updateIsPinnedList(true);
+    }
+
+    return response;
+
+  }
+
+  Future<Map<String, dynamic>> unpin() async {
+
+    final response = await _callPinRequest();
+
+    if (response['status_code'] == 200) {
+      _updateIsPinnedList(false);
+    }
+
+    return response;
+
+  }
+
+  Future<Map<String, dynamic>> _callPinRequest() async {
+
     final response = await ApiClient.post(ApiPath.pinVent, {
       'pinned_by': userProvider.user.username,
       'post_id': postId
     });
 
-    if (response.statusCode == 201) {
-      _updateIsPinnedList(true);
-    }
-
     return {
       'status_code': response.statusCode
     };
-
-  }
-
-  Future<void> unpin() async {
-
-    const query = 'DELETE FROM ${TableNames.pinnedVentInfo} WHERE post_id = :post_id AND pinned_by = :pinned_by';
-
-    final params = {
-      'pinned_by': userProvider.user.username,
-      'post_id': postId
-    };
-
-    await executeQuery(query, params).then(
-      (_) => _updateIsPinnedList(false)
-    );
 
   }
 
