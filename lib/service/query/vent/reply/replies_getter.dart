4c9e9bdd7970a6689edebc1a69a4c4e8aa45ecae
Tho100 <nfrealyt@gmail.com>
2025-10-01 13:36:53 +0800

    Make request to backend to get replies

--- lib/service/query/vent/reply/replies_getter.dart
+++ lib/service/query/vent/reply/replies_getter.dart
@@ -1,10 +1,10 @@
-import 'package:revent/global/table_names.dart';
 import 'package:revent/helper/data_converter.dart';
+import 'package:revent/shared/api/api_client.dart';
+import 'package:revent/shared/api/api_path.dart';
 import 'package:revent/shared/provider_mixins.dart';
 import 'package:revent/service/query/general/base_query_service.dart';
 import 'package:revent/helper/extract_data.dart';
 import 'package:revent/helper/format_date.dart';
-import 'package:revent/service/query/general/replies_id_getter.dart';
 
 class RepliesGetter extends BaseQueryService with UserProfileProviderService, VentProviderService {
 
@@ -14,102 +14,39 @@ class RepliesGetter extends BaseQueryService with UserProfileProviderService, Ve
 
   Future<Map<String, List<dynamic>>> getReplies() async {
 
-    final repliesIds = await ReplyIdGetter(commentId: commentId).getAllRepliesId();
-
-    const getRepliesQuery = 
-    '''
-      SELECT 
-        cri.reply, 
-        cri.replied_by,
-        cri.created_at,
-        cri.total_likes, 
-        upi.profile_picture 
-      FROM ${TableNames.commentRepliesInfo} cri 
-      JOIN ${TableNames.userProfileInfo} upi
-        ON cri.replied_by = upi.username 
-      WHERE cri.comment_id = :comment_id 
-        AND NOT EXISTS (
-          SELECT 1
-          FROM ${TableNames.userBlockedInfo} ubi
-          WHERE 
-            ubi.blocked_by = :blocked_by AND 
-            ubi.blocked_username = cri.replied_by    
-        )
-    ''';
-
-    final param = {
+    final response = await ApiClient.post(ApiPath.repliesGetter, {
       'comment_id': commentId,
+      'creator': activeVentProvider.ventData.creator,
+      'current_user': userProvider.user.username,
       'blocked_by': userProvider.user.username
-    };
+    });
 
-    final results = await executeQuery(getRepliesQuery, param);
+    final replies = ExtractData(data: response.body!['replies']);
 
-    final extractedData = ExtractData(rowsData: results);
+    final reply = replies.extractColumn<String>('reply');
+    final repliedBy = replies.extractColumn<String>('replied_by');
+    final totalLikes = replies.extractColumn<int>('total_likes');
 
-    final reply = extractedData.extractStringColumn('reply');
-    final repliedBy = extractedData.extractStringColumn('replied_by');
-    final totalLikes = extractedData.extractIntColumn('total_likes');
-
-    final replyTimestamp = FormatDate().formatToPostDate(
-      data: extractedData, columnName: 'created_at'
+    final replyTimestamp = FormatDate().formatToPostDate2(
+      replies.extractColumn<String>('created_at')
     );
 
     final profilePictures = DataConverter.convertToPfp(
-      extractedData.extractStringColumn('profile_picture')
+      replies.extractColumn<String>('created_at')
     );
 
-    final isLikedState = await _replyLikedState(
-      commentId: commentId,
-      isLikedByCreator: false,
-      repliesIds: repliesIds,
-    );
-
-    final isLikedByCreatorState = await _replyLikedState(
-      commentId: commentId,
-      isLikedByCreator: true,
-      repliesIds: repliesIds,
-    );
+    final isLiked = replies.extractColumn<bool>('is_liked');
+    final isLikedByCreator = replies.extractColumn<bool>('is_liked_by_creator');
 
     return {
       'replied_by': repliedBy,
       'reply': reply,
       'reply_timestamp': replyTimestamp,
       'total_likes': totalLikes,
-      'is_liked': isLikedState,
-      'is_liked_by_creator': isLikedByCreatorState,
+      'is_liked': isLiked,
+      'is_liked_by_creator': isLikedByCreator,
       'profile_picture': profilePictures,
     };
-
-  }
-
-  Future<List<bool>> _replyLikedState({
-    required int commentId,
-    required bool isLikedByCreator,
-    required List<int> repliesIds,
-  }) async {
-
-    const readLikesQuery = 
-    '''
-      SELECT rli.reply_id
-      FROM ${TableNames.repliesLikesInfo} rli
-      JOIN ${TableNames.commentRepliesInfo} cpi
-        ON rli.reply_id = cpi.reply_id
-      WHERE rli.liked_by = :liked_by AND cpi.comment_id = :comment_id
-    ''';
-
-    final params = {
-      'liked_by': isLikedByCreator ? activeVentProvider.ventData.creator : userProvider.user.username,
-      'comment_id': commentId
-    };
-
-    final results = await executeQuery(readLikesQuery, params);
-
-    final extractedIds = ExtractData(rowsData: results).extractIntColumn('reply_id');
-
-    return List<bool>.generate(
-      repliesIds.length,
-      (i) => extractedIds.contains(repliesIds[i]),
-    );
 
   }
 
--- lib/shared/api/api_path.dart
+++ lib/shared/api/api_path.dart
@@ -35,10 +35,10 @@ class ApiPath {
   static const vaultVentsGetter = '$ventsGetter/vault';
 
   static const createComment = '/create-comment';
-
   static const commentActions = '/comment-actions';
   static const deleteComment = '$commentActions/delete-comment';
-
   static const commentsGetter = '/comments-getter';
+
+  static const repliesGetter = '/replies-getter';
 
 }
