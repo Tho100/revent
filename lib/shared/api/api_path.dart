d8bb2453cf56a73100128bdcddcc29e1c818cf6c
Tho100 <nfrealyt@gmail.com>
2025-10-09 13:22:16 +0800

    Makes api-path top variable private

--- lib/shared/api/api_path.dart
+++ lib/shared/api/api_path.dart
@@ -6,61 +6,63 @@ class ApiPath {
   static const verifyUser = '/verify-user';
   static const verifyUserAuth = '$verifyUser/verify-auth';
 
-  static const updateUser = '/update-user';
-  static const updateUserAuth = '$updateUser/update-auth';
-  static const updateUserAvatar = '$updateUser/update-avatar';
+  static const _updateUser = '/update-user';
+  static const updateUserAuth = '$_updateUser/update-auth';
+  static const updateUserAvatar = '$_updateUser/update-avatar';
 
-  static const userProfile = '/user-profile';
-  static const userProfileInfoGetter = '$userProfile/get-info';
-  static const userAvatarGetter = '$userProfile/get-avatar';
+  static const _userProfile = '/user-profile';
+  static const userProfileInfoGetter = '$_userProfile/get-info';
+  static const userAvatarGetter = '$_userProfile/get-avatar';
 
-  static const createVent = '/create-vent';
-  static const createDefaultVent = '$createVent/default-vent';
-  static const createVaultVent = '$createVent/vault-vent';
+  static const _createVent = '/create-vent';
+  static const createDefaultVent = '$_createVent/default-vent';
+  static const createVaultVent = '$_createVent/vault-vent';
 
-  static const ventActions = '/vent-actions';
-  static const deleteVent = '$ventActions/delete-vent';
-  static const deleteVaultVent = '$ventActions/delete-vault-vent';
-  static const likeVent = '$ventActions/like-vent';
-  static const saveVent = '$ventActions/save-vent';
-  static const pinVent = '$ventActions/pin-vent';
-// TODO: make top variable lke ventsGetter private
-  static const ventsGetter = '/vents-getter';
-  static const latestVentsGetter = '$ventsGetter/latest';
-  static const trendingVentsGetter = '$ventsGetter/trending';
-  static const followingVentsGetter = '$ventsGetter/following';
-  static const searchVentsGetter = '$ventsGetter/search';
-  static const likedVentsGetter = '$ventsGetter/liked';
-  static const savedVentsGetter = '$ventsGetter/saved';
-  static const vaultVentsGetter = '$ventsGetter/vault';
+  static const _ventActions = '/vent-actions';
+  static const deleteVent = '$_ventActions/delete-vent';
+  static const deleteVaultVent = '$_ventActions/delete-vault-vent';
+  static const likeVent = '$_ventActions/like-vent';
+  static const saveVent = '$_ventActions/save-vent';
+  static const pinVent = '$_ventActions/pin-vent';
 
-  static const ventInfoGetter = '/vent-info-getter';
-  static const ventBodyTextGetter = '$ventInfoGetter/body-text';
-  static const ventMetadataGetter = '$ventInfoGetter/metadata';
-  static const vaultBodyTextGetter = '$ventInfoGetter/vault-body-text';
+  static const _ventsGetter = '/vents-getter';
+  static const latestVentsGetter = '$_ventsGetter/latest';
+  static const trendingVentsGetter = '$_ventsGetter/trending';
+  static const followingVentsGetter = '$_ventsGetter/following';
+  static const searchVentsGetter = '$_ventsGetter/search';
+  static const likedVentsGetter = '$_ventsGetter/liked';
+  static const savedVentsGetter = '$_ventsGetter/saved';
+  static const vaultVentsGetter = '$_ventsGetter/vault';
+
+  static const _ventInfoGetter = '/vent-info-getter';
+  static const ventBodyTextGetter = '$_ventInfoGetter/body-text';
+  static const ventMetadataGetter = '$_ventInfoGetter/metadata';
+  static const vaultBodyTextGetter = '$_ventInfoGetter/vault-body-text';
 
   static const createComment = '/create-comment';
-  static const commentActions = '/comment-actions';
-  static const deleteComment = '$commentActions/delete-comment';
-  static const likeComment = '$commentActions/like-comment';
   static const commentsGetter = '/comments-getter';
 
+  static const _commentActions = '/comment-actions';
+  static const deleteComment = '$_commentActions/delete-comment';
+  static const likeComment = '$_commentActions/like-comment';
+
   static const createReply = '/create-reply';
-  static const replyActions = '/reply-action';
-  static const deleteReply = '$replyActions/delete-reply';
-  static const likeReply = '$replyActions/like-reply';
   static const repliesGetter = '/replies-getter';
+  
+  static const _replyActions = '/reply-action';
+  static const deleteReply = '$_replyActions/delete-reply';
+  static const likeReply = '$_replyActions/like-reply';
 
-  static const profilePostsGetter = '/profile-posts-getter';
-  static const profileOwnPostsGetter = '$profilePostsGetter/posts';
-  static const profileSavedPostsGetter = '$profilePostsGetter/saved';
+  static const _profilePostsGetter = '/profile-posts-getter';
+  static const profileOwnPostsGetter = '$_profilePostsGetter/posts';
+  static const profileSavedPostsGetter = '$_profilePostsGetter/saved';
 
-  static const searchGetter = '/search-getter';
-  static const searchProfilesGetter = '$searchGetter/profiles';
+  static const _searchGetter = '/search-getter';
+  static const searchProfilesGetter = '$_searchGetter/profiles';
 
-  static const activityGetter = '/activity-getter';
-  static const activityFollowersGetter = '$activityGetter/followers';
-  static const activityRecentLikesGetter = '$activityGetter/recent-likes';
-  static const activityAllTimeLikesGetter = '$activityGetter/all-time-likes';
+  static const _activityGetter = '/activity-getter';
+  static const activityFollowersGetter = '$_activityGetter/followers';
+  static const activityRecentLikesGetter = '$_activityGetter/recent-likes';
+  static const activityAllTimeLikesGetter = '$_activityGetter/all-time-likes';
 
 }
