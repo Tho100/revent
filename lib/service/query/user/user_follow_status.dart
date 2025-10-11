065d150768f641c6d5d04986bd398edac6d0b156
Tho100 <nfrealyt@gmail.com>
2025-10-11 21:00:14 +0800

    Make request to backend to get user-following-status

--- lib/service/query/user/user_follow_status.dart
+++ lib/service/query/user/user_follow_status.dart
@@ -1,23 +1,19 @@
-import 'package:revent/global/table_names.dart';
 import 'package:revent/helper/get_it_extensions.dart';
 import 'package:revent/main.dart';
 import 'package:revent/service/query/general/base_query_service.dart';
+import 'package:revent/shared/api/api_client.dart';
+import 'package:revent/shared/api/api_path.dart';
 
 class UserFollowStatus extends BaseQueryService {
-// TODO: make request to backend
+
   Future<bool> isFollowing({required String username}) async {
 
-    const query = 
-      'SELECT 1 FROM ${TableNames.userFollowsInfo} WHERE following = :following AND follower = :follower LIMIT 1';
-      
-    final param = {
+    final response = await ApiClient.post(ApiPath.userFollowingStatusGetter, {
       'following': username,
-      'follower': getIt.userProvider.user.username
-    };
+      'current_user': getIt.userProvider.user.username
+    });
 
-    final results = await executeQuery(query, param);
-
-    return results.rows.isNotEmpty;
+    return response.body!['is_following'];
 
   }
 
