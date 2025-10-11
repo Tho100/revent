8499f2f278ad6a4c0180ac7e41fdbb9eae9b1e88
Tho100 <nfrealyt@gmail.com>
2025-10-11 21:10:36 +0800

    Updated follow-suggestion-getter return type and added TODOs

--- lib/service/query/user/user_follow_status.dart
+++ lib/service/query/user/user_follow_status.dart
@@ -3,7 +3,7 @@ import 'package:revent/main.dart';
 import 'package:revent/service/query/general/base_query_service.dart';
 import 'package:revent/shared/api/api_client.dart';
 import 'package:revent/shared/api/api_path.dart';
-
+// TODO: rename to UserFollowingStatus
 class UserFollowStatus extends BaseQueryService {
 
   Future<bool> isFollowing({required String username}) async {
