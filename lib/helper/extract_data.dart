0171385ef8f3f38a00496f216f6682ff7a9e3c5d
Tho100 <nfrealyt@gmail.com>
2025-10-24 14:41:33 +0800

    Removed unused functions on ExtractData

--- lib/helper/extract_data.dart
+++ lib/helper/extract_data.dart
@@ -1,24 +1,8 @@
-import 'package:mysql_client/mysql_client.dart';
-
 class ExtractData {
 
   List<dynamic>? data;
-  IResultSet? rowsData;
 
-  ExtractData({this.data, this.rowsData});
-
-// TODO: Get rid of this function
-
-  /// Extracts a list of [int] values from the specified [columnName].
-  /// 
-  /// Parses string values to integers, defaulting to 0 if parsing fails.
-   
-  List<int> extractIntColumn(String columnName) {
-    return rowsData!.rows.map((row) {
-      final value = row.assoc()[columnName]!;
-      return int.tryParse(value) ?? 0;
-    }).toList();
-  }
+  ExtractData({this.data});
 
   /// Returns all values for [header] from [data] as a List<T>.
 
