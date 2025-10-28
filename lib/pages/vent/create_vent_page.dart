854233f285156d4126b95bb6d2f6f6c8f4de2b3b
Tho100 <nfrealyt@gmail.com>
2025-10-28 13:37:05 +0800

    Removed unused VentChecker class

--- lib/pages/vent/create_vent_page.dart
+++ lib/pages/vent/create_vent_page.dart
@@ -22,7 +22,6 @@ import 'package:revent/shared/widgets/ui_dialog/snack_bar.dart';
 import 'package:revent/service/query/vent/create_new_item.dart';
 import 'package:revent/shared/themes/theme_color.dart';
 import 'package:revent/shared/widgets/ui_dialog/alert_dialog.dart';
-import 'package:revent/service/query/vent/vent_checker.dart';
 import 'package:revent/shared/widgets/app_bar.dart';
 import 'package:revent/shared/widgets/buttons/custom_outlined_button.dart';
 import 'package:revent/shared/widgets/buttons/sub_button.dart';
@@ -81,17 +80,6 @@ class _CreateVentPageState extends State<CreateVentPage> with
     }
 
     try {
-
-      final ventChecker = VentChecker(title: ventTitle);
-
-      final isVentAlreadyExists = vaultVentNotifier.value 
-        ? await ventChecker.isVaultVentExists()
-        : await ventChecker.isVentExists();
-
-      if (isVentAlreadyExists) {
-        CustomAlertDialog.alertDialog(AlertMessages.postTitleExists);
-        return;
-      }
 
       if (vaultVentNotifier.value) {
         await _createVaultVent(title: ventTitle, bodyText: ventBodyText, tags: tags).then(
